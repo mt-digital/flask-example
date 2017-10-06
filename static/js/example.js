b5468f5ec8bb58a73e6de47da97be7c16fb5fe6b
Matthew Turner <maturner01@gmail.com>
2017-10-05 17:19:59 -0700

    fixed formatting in example.js

--- static/js/example.js
+++ static/js/example.js
@@ -2,12 +2,11 @@
  * Here you'll write code to make a GET request to your API
  */
 function displayData(data) {
-            // render data somewhere in website
-            Object.keys(data).some(function(key, idx) {
+  // render data somewhere in website
+  Object.keys(data).some(function(key, idx) {
 
-              $('div#put-stuff-here').append('<p>' + key + '</p>');
-            });
-          }
+    $('div#put-stuff-here').append('<p>' + key + '</p>');
+  });
+}
 
-$.getJSON("http://localhost:5000/my-route",
-          displayData);
+$.getJSON("http://localhost:5000/my-route", displayData);
