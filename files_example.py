c686a9023748980e297bd31f05144d1fb73108c5
Matthew Turner <maturner01@gmail.com>
2017-11-16 18:15:02 -0800

    added comments to files_example

--- files_example.py
+++ files_example.py
@@ -12,18 +12,22 @@ import os
 
 dirname = 'files_dir'
 
-# check if directory exists
+# Check if directory exists.
 if not os.path.isdir(dirname):
-    # if it does not, create it
+    # If it does not, create it.
     os.mkdir(dirname)
 
-
+# See how many files are in files_dir.
 idx = len(os.listdir(dirname))
+# Convert that number to a string.
 idx_str = str(idx)
-
+# Some fake text to go in our new file..
 file_text = 'This is an example file.\n'
-
+# Create a filename; note this depends on no other person/program.
+# Putting a file with a name like <integer>.txt.
 filename = os.path.join(dirname, idx_str + '.txt')
 
+# This is the most common way to handle writing to files in Python.
+# This is also the most idiomatic Python-"Pythonic" for short.
 with open(filename, 'w+') as f:
     f.write(file_text)
